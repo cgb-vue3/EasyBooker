25c38d10726442d76e94629369efeb69852665b6
duxiaodo <2088867510@qq.com>
2021-03-16 18:44:52 +0800

    GloabalHeader 用静态登录数据测试登录状态切换

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -2,7 +2,7 @@
 	<nav class="navbar navbar-light bg-white">
 		<div class="container-fluid">
 			<a class="navbar-brand text-primary">U-LOG</a>
-			<form class="d-flex">
+			<form class="d-flex" v-if="!User.isLogin">
 				<button type="button" class="btn btn-outline-primary btn-sm me-2 px-3">
 					登录
 				</button>
@@ -10,15 +10,50 @@
 					注册
 				</button>
 			</form>
+			<drop-down :title="User.name" v-else>
+				<drop-down-item>
+					编辑
+				</drop-down-item>
+				<drop-down-item>
+					我的专栏
+				</drop-down-item>
+				<drop-down-item>
+					退出
+				</drop-down-item>
+			</drop-down>
 		</div>
 	</nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType, ref } from 'vue'
+import DropDown from './DropDown.vue'
+import DropDownItem from './DropDownItem.vue'
 
+export interface UserProps {
+	isLogin: boolean;
+	name: string;
+	id?: number;
+}
 export default defineComponent({
-	name: 'App',
+	name: 'GlobalHeader',
+	components: {
+		DropDownItem,
+		DropDown
+	},
+	props: {
+		user: {
+			type: Object as PropType<UserProps>,
+			required: true
+		}
+	},
+	setup(props) {
+		const User = ref(props.user)
+
+		return {
+			User
+		}
+	}
 })
 </script>
 
