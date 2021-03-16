e6c6625ee577fa2d60ba6d469b4ba653d32fa2ae
duxiaodo <2088867510@qq.com>
2021-03-16 09:44:05 +0800

    DropDown 组件

---
+++ src/components/DropDownItem.vue
@@ -0,0 +1,13 @@
+<template>
+	<li>
+		<a class="dropdown-item" href="#">
+            <slot></slot>
+        </a>
+	</li>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
