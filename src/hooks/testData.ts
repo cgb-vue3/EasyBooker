7f131d06e42f74ece604004020a5b6ff6612ed45
duxiaodo <2088867510@qq.com>
2021-03-17 13:41:45 +0800

    ColumnDetail 显示Posts

--- src/hooks/testData.ts
+++ src/hooks/testData.ts
@@ -1,5 +1,5 @@
 export const Columns = [{
-    "_id": "5f3e86d62c56ee13bb83096c",
+    "_id": "5f3e86d62c56ee13bb830961c",
     "title": "铺先生",
     "description": "酒店售货机",
     "author": "5f3e86d62c56ee13bb830961",
@@ -14,7 +14,7 @@ export const Columns = [{
     "__v": 0,
     "createdAt": "2020-08-20T14:21:10.711Z"
 }, {
-    "_id": "5f3e86d62c56ee13bb83096c",
+    "_id": "5f3e86d62c56ee13bb830962c",
     "title": "铺先生",
     "description": "酒店售货机，酒店经营，共享经济，新零售，共享数据线，关注灵趣科技，了解更多",
     "author": "5f3e86d62c56ee13bb830961",
@@ -30,7 +30,7 @@ export const Columns = [{
     "__v": 0,
     "createdAt": "2020-08-20T14:21:10.711Z"
 },{
-    "_id": "5f3e86d62c56ee13bb83096c",
+    "_id": "5f3e86d62c56ee13bb830963c",
     "title": "铺先生",
     "description": "酒店售货机，酒店经营，共享经济，新零售，共享数据线，关注灵趣科技，了解更多",
     "author": "5f3e86d62c56ee13bb830961",
@@ -46,7 +46,7 @@ export const Columns = [{
     "__v": 0,
     "createdAt": "2020-08-20T14:21:10.711Z"
 }, {
-    "_id": "5f3e86d62c56ee13bb83096c",
+    "_id": "5f3e86d62c56ee13bb830964c",
     "title": "铺先生",
     "description": "酒店售货机，酒店经营，共享经济，新零售，共享数据线，关注灵趣科技，了解更多",
     "author": "5f3e86d62c56ee13bb830961",
@@ -63,7 +63,7 @@ export const Columns = [{
     "createdAt": "2020-08-20T14:21:10.711Z"
 },
 {
-    "_id": "5f3e86d62c56ee13bb83096c",
+    "_id": "5f3e86d62c56ee13bb830965c",
     "title": "铺先生",
     "description": "酒店售货机，酒店经营，共享经济，新零售，共享数据线，关注灵趣科技，了解更多",
     "author": "5f3e86d62c56ee13bb830961",
@@ -79,7 +79,7 @@ export const Columns = [{
     "__v": 0,
     "createdAt": "2020-08-20T14:21:10.711Z"
 }, {
-    "_id": "5f3e86d62c56ee13bb83096c",
+    "_id": "5f3e86d62c56ee13bb830966c",
     "title": "铺先生",
     "description": "酒店售货机，酒店经营，共享经济，新零售，共享数据线，关注灵趣科技，了解更多",
     "author": "5f3e86d62c56ee13bb830961",
@@ -95,3 +95,84 @@ export const Columns = [{
     "__v": 0,
     "createdAt": "2020-08-20T14:21:10.711Z"
 }]
+
+export const Posts = [
+    {
+      "createdAt": "2020-08-21 19:20:00",
+      "image": {
+        "_id": "5e912f4c7f24af242a382b8c",
+        "url": "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5e912f4c7f24af242a382b8c.png"
+      },
+      "column": "5f3e86d62c56ee13bb83096c",
+      "author": "5f3e86d62c56ee13bb830961",
+      "excerpt": "我在学校学语法的时候，最让我头疼的就是各种语法的专有名词，状语，同位语，动名词，动词分词，这些听上去很高大上的称谓到底是什么？这篇文章会分三次推送给，先从什么是状语说起，然后再深入聊聊if的正常语态和虚拟语态句型。 英语中最早学习的复杂句型…",
+      "title": "英语语法的第一个坑if从句，你掉进去了吗？（一）",
+      "_id": "5f3fade0c9875c2cd848a2cb",
+      "key": 0
+    },
+    {
+      "createdAt": "2020-08-21 19:20:00",
+      "image": {
+        "_id": "5e913eb3912836262645f30f",
+        "url": "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5e913eb3912836262645f30f.jpg"
+      },
+      "column": "5f3e86d62c56ee13bb83096c",
+      "author": "5f3e86d62c56ee13bb830961",
+      "excerpt": "语法速成乍一听很像卖假药的小广告，但严格来说，在满足了各种条件下，英语语法速成并不是不可能的。 首先“速成“的定义到底是什么？如果是指流利使用正确的语法表达意思，那就是纯扯淡，因为所有的技能都需要大量练习时间，”一万小时定律“是经过千百万…",
+      "title": "五分钟入门英语语法可能吗？",
+      "_id": "5f3fade0c9875c2cd848a2cc",
+      "key": 1
+    },
+    {
+      "createdAt": "2020-08-21 19:20:00",
+      "image": {
+        "_id": "5f33a5bb16aa5bb1ac376968",
+        "url": "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f33a5bb16aa5bb1ac376968.png"
+      },
+      "column": "5f3e86d62c56ee13bb83096c",
+      "author": "5f3e86d62c56ee13bb830961",
+      "excerpt": "关于这个问题，我想纯粹分享一下我个人的经验，仅供各位参考。 我是小学三年级开始学英语的，那个时候学校里还没有正式开始上英语课，但组织了一个课外兴趣班，每周上三个晚上。估计即使是90年代，家长也有不想让孩子输在起跑线的模糊意识吧。不过那个兴趣…",
+      "title": "学英语是靠刷一百道题还是看一百部电影？",
+      "_id": "5f3fade0c9875c2cd848a2c8",
+      "key": 2
+    },
+    {
+      "createdAt": "2020-08-21 19:20:00",
+      "image": {
+        "_id": "5e913eb3912836262645f30f",
+        "url": "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5e913eb3912836262645f30f.jpg"
+      },
+      "column": "5f3e86d62c56ee13bb83096c",
+      "author": "5f3e86d62c56ee13bb830961",
+      "excerpt": "也许你有这样的困扰：所有英语老师都告诉你学英语需要多听多看，为什么看了几年美剧后，眼睛还是得死盯着字幕才能听懂对白呢?说实话，我看日剧也是这个感觉。 我大学二外选择的是日语，日语的基础语法学过一些，词汇也背过一些。我日剧也看了有十几年了吧，…",
+      "title": "看了这么多年美剧，我为什么还是没法摆脱字幕？",
+      "_id": "5f3fade0c9875c2cd848a2c9",
+      "key": 3
+    },
+    {
+      "createdAt": "2020-08-21 19:20:00",
+      "image": {
+        "_id": "5e913ea8912836262645f30e",
+        "url": "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5e913ea8912836262645f30e.png"
+      },
+      "column": "5f3e86d62c56ee13bb83096c",
+      "author": "5f3e86d62c56ee13bb830961",
+      "excerpt": "上一篇我们说了if条件句是触发主线动作的一个开关，即满足了条件就触发动作，没满足就不触发，条件本身是个客观存在，没有时间属性，所以用一般现在时。 接下来就要开始复杂起来了：因为有时候if条件句也可以加上时间属性。 中国学生学习时态有个很大的 误区…",
+      "title": "英语语法的第一个坑if从句，你掉进去了吗？（二）",
+      "_id": "5f3fade0c9875c2cd848a2ca",
+      "key": 4
+    },
+    {
+      "createdAt": "2020-08-21 19:20:00",
+      "image": {
+        "_id": "5e912f4c7f24af242a382b8c",
+        "url": "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5e912f4c7f24af242a382b8c.png"
+      },
+      "column": "5f3e86d62c56ee13bb83096c",
+      "author": "5f3e86d62c56ee13bb830961",
+      "excerpt": "昨天一个高中学生跟我说他发现别的同学做阅读的速度比他快，认识的单词也比他多。而他感觉自己一直在原地踏步，因此很挫败，心态甚至有点崩了。在我看来，他是一个很努力的学生，每天做作业做到半夜，一个人已经很努力了但还是看不到自己进步，确实会很挫伤…",
+      "title": "一个高中生的烦恼（也是人生的难题)",
+      "_id": "5f3fade0c9875c2cd848a2c7",
+      "key": 5
+    }
+  ]
--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,18 +1,51 @@
 <template>
-    <pre>
-        {{ route }}
-    </pre>
+	<div class="container w-75 column-detail">
+		<div class="d-flex p-3 mb-4 border-bottom">
+			<img :src="column.avatar.url" :alt="column.title" class="rounded-circle column-avatar" />
+			<div class="card-content">
+				<h5 class="card-title">{{ column.title }}</h5>
+				<p class="card-text">
+					{{ column.description + '...' }}
+				</p>
+			</div>
+		</div>
+
+		<post-list :Posts="Posts"></post-list>
+	</div>
 </template>
 <script>
-import { defineComponent } from "vue";
-import {useRoute} from 'vue-router'
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+import PostList from '../components/PostList.vue'
+import {Columns, Posts} from '../hooks/testData'
+
 
 export default defineComponent({
-    setup() {
+	components: {
+        PostList,
+    },
+	setup() {
         const route = useRoute()
-        return {
-            route
-        }
-    }
+        const column = Columns.filter(column => column._id == route.params.id)[0]
+
+		return {
+            route,
+            column,
+            Posts
+		}
+	},
 })
 </script>
+<style lang="scss" scoped>
+.column-detail {
+
+    .column-avatar {
+        box-sizing: border-box;
+        // padding: 20px 20px 0 0 ;
+        margin-right: 30px;
+        width: 150px;
+        height: 150px;
+    }
+}
+
+</style>
