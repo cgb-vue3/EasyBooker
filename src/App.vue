cc9930136b46ce279757bbc13e884e14207b7a8f
duxiaodo <2088867510@qq.com>
2021-03-18 11:28:31 +0800

    全局的Loading 组件

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
-	<div class="bg-white border-bottom">
-		<div class="container w-75">
-			<GlobalHeader :user="user" />
+	<div id="App">
+		<div class="bg-white border-bottom">
+			<div class="container w-75">
+				<GlobalHeader :user="user" />
+			</div>
 		</div>
-	</div>
 
-	<div class="container bg-white w-75 mt-3 shadow-lg p-3">
-		<router-view></router-view>
+		<div class="container bg-white w-75 mt-3 shadow-lg p-3 main" v-show="!isLoading">
+			<router-view></router-view>
+		</div>
+		<loading v-if="isLoading"></loading>
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue'
 import GlobalHeader from './components/GlobalHeader.vue'
+import Loading from './components/Loading.vue'
+
 import { useStore } from 'vuex'
 
 export default defineComponent({
 	name: 'App',
 	components: {
 		GlobalHeader,
+		Loading,
 	},
 	setup() {
-		const user = computed(() => useStore().state.user)
+		const store = useStore()
+		const user = computed(() => store.state.user)
+		const isLoading = computed(() => store.state.loading)
+
 		return {
 			user,
+			isLoading,
 		}
 	},
 })
@@ -33,5 +43,9 @@ export default defineComponent({
 #app {
 	background-color: #f4f5f5;
 	padding-bottom: 100px;
+
+	.main {
+		min-height: 100vh;
+	}
 }
 </style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="text-center loading">
+		<div class="spinner-border text-primary" role="status">
+			<span class="visually-hidden">Loading...</span>
+		</div>
+	</div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+	name: 'Loading',
+})
+</script>
+<style lang="scss" scoped>
+.loading {
+    position: fixed;
+    top: 0;
+    left: 0;
+	width: 100vw;
+	height: 100vh; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.2);
+}
+</style>
